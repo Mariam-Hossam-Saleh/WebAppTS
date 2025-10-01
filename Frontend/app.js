0da4a2bd4a53f758fef9f978fb63df0bd718949c
Mariam-Hossam-Saleh <mariam.hossam.keshk@gmail.com>
2025-10-01 12:59:49 +0300

    Replacing the Backend URI

--- Frontend/app.js
+++ Frontend/app.js
@@ -6,6 +6,7 @@ async function loadTodos() {
   const todos = await res.json();
   const list = document.getElementById("todo-list");
   list.innerHTML = "";
+  
   todos.forEach(todo => {
     const li = document.createElement("li");
     li.textContent = todo.text + (todo.done ? " ✅" : "");
